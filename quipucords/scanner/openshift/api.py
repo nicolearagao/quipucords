79807338c27049edb1aa45f0f3ff43131224db00
Bruno Ciconelle <bciconel@redhat.com>
2022-12-21 18:54:51 -0300

    third

--- quipucords/scanner/openshift/api.py
+++ quipucords/scanner/openshift/api.py
@@ -73,7 +73,10 @@ class OpenShiftApi:
         """Initialize OpenShiftApi."""
         configuration.verify_ssl = ssl_verify
         self._api_client = ApiClient(configuration=configuration)
-        self._dynamic_client = DynamicClient(self._api_client)
+
+    @cached_property
+    def _dynamic_client(self):
+        return DynamicClient(self._api_client)
 
     @classmethod
     def from_auth_token(
