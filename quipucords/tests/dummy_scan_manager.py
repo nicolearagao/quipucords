c43ed70b1604a2c54a4c17423a074b91c890ae50
Bruno Ciconelle <bciconel@redhat.com>
2022-12-22 12:29:39 -0300

    tests: add DummyScanManager
    Replace actual ScanManager in tests with a dummy scan manager instance.
    
    Drop tests_manager as this test would require the actual ScanManager to make
    sense.
    
    Related to DISCOVERY-50

---
+++ quipucords/tests/dummy_scan_manager.py
@@ -0,0 +1,39 @@
+class SingletonMeta(type):
+    """
+    Metaclass designed to force classes to behave as singletons.
+    
+    Shamelesly copied from 
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class DummyScanManager(metaclass=SingletonMeta):
+    def __init__(self):
+        self._queue = []
+
+    def put(self, job):
+        self._queue.append(job)
+
+    def is_alive(self):
+        return True
+
+    def work(self):
+        while self._queue:
+            current_job = self._queue.pop()
+            current_job.start()
+            while current_job.exitcode is None:
+                ...
+
+    def kill(self, job, command):
+        ...
